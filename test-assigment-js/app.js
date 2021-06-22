5e69c487c2850935d76bc5205c6f45ace51f8085
Veronika <nesteryuk15@gmail.com>
2021-06-22 23:25:58 +0300

    raw slider

--- test-assigment-js/app.js
+++ test-assigment-js/app.js
@@ -7,7 +7,17 @@ toggleSwitch.addEventListener('click', () => {
 }); 
 
 // range slider
+const x = document.getElementById('input');
+const y = document.getElementById('output');
 
+const a = 100;
+const b = Math.pow(a, 1/a);
+
+function updateOutput(event) {
+  y.innerText = Math.floor(a * Math.pow(b, x.value));
+}
+updateOutput();
+x.addEventListener('mousemove', updateOutput);
 
 
 
