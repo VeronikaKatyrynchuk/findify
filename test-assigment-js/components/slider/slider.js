9719d313b2f85956d6f559891301919ff6892a3b
Veronika <nesteryuk15@gmail.com>
2021-06-23 15:22:56 +0300

    done
    - components
    -slider
    -intro

--- test-assigment-js/components/slider/slider.js
+++ test-assigment-js/components/slider/slider.js
@@ -1,13 +1,3 @@
-const toggleSwitch = document.getElementById('input');
-const card = document.getElementById('card');
-
-
-toggleSwitch.addEventListener('click', () => {
-    card.classList.toggle('active');
-}); 
-
-
-// slider
 const slider = document.getElementById('range');
 const output = document.getElementById('input-value');
 
@@ -16,4 +6,3 @@ output.innerHTML = slider.value;
 slider.oninput = function() {
   output.innerHTML = this.value;
 };
-
---
+++ test-assigment-js/components/switch/switch.js
@@ -0,0 +1,7 @@
+const toggleSwitch = document.getElementById('input');
+const card = document.getElementById('card');
+
+
+toggleSwitch.addEventListener('click', () => {
+    card.classList.toggle('active');
+}); 
